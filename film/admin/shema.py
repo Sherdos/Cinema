00d2a3efc650c337240ae19833399b87b1f9fc7c
sherdos <sherdos26@gmail.com>
2023-08-28 17:24:53 +0600

    v1.0

--- film/admin/shema.py
+++ film/admin/shema.py
@@ -15,3 +15,20 @@ movie = (
         'fields': (('url',),)
     }),
 )
+serial = (
+    (None, {
+        'fields': (('title'),)
+    }),
+    (None, {
+        'fields': ('description', ('poster', 'get_image'))
+    }),
+    (None, {
+        'fields': (('year'),)
+    }),
+    (None, {
+        'fields': (('genres', 'category'),)
+    }),
+    ('Options', {
+        'fields': (('url',),)
+    }),
+)
